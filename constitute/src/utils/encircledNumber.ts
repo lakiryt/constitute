2a2b89ed1cf188e3817534d32f37f236d34d302d
Taro Yoshioka <lakiryt@github.com>
2026-05-04 18:58:53 +0900

    微修正

--- constitute/src/utils/encircledNumber.ts
+++ constitute/src/utils/encircledNumber.ts
@@ -1,4 +1,4 @@
 export function formatEncircledNumber(value: number): string {
-    // ⓪のUnicodeコードポイントをvalue分だけ進める
+    // ①のUnicodeコードポイントをvalue-1だけ進める
     return String.fromCodePoint("①".codePointAt(0)! + value - 1);
 }
